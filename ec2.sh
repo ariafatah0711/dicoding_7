22632f17e1fb61a50c0f848e47ee0b13b2bb373c
ariafatah0711 <ariafatah0711@gmail.com>
2025-01-27 14:47:30 +0700

    update

--- ec2.sh
+++ ec2.sh
@@ -2,7 +2,7 @@ new_user=app_python
 pass="YXJpYQo="
 # pass="data"
 
-sudo useradd app_python
+sudo useradd -m -s /bin/bash $new_user
 echo "$new_user:$pass" | sudo chpasswd
 echo "PasswordAuthentication yes" | sudo tee /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 sudo systemctl restart ssh
